9e0af02d18e6cb3009773a8e4d5240585eddb557
nanogutierrez <105751221+nanogutierrez@users.noreply.github.com>
2022-08-30 23:17:01 -0300

    Add files via upload

---
+++ NavBar.js
@@ -0,0 +1,11 @@
+import logo from '../logo.svg';
+const NavBar = () => {
+    return (
+    <div className="navbar-container">
+        <img src={logo} className="App-logo" alt="logo" />
+        Tentatortas
+        </div>
+        );
+};
+
+export default NavBar;
